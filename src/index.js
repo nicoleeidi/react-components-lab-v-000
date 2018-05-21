35b807268ad0d9bf9ce38b73efba1100f6dc963b
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-21 21:47:48 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ const BUTCHER_PRODUCTS = [
 export class OlderCoaster extends React.Component {
   
   // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
+  render() {
+    return (
+      <div>Hello!</div>
+    )
+  }
   // 
 }
 
