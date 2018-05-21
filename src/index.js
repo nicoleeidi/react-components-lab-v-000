5ffc8ce4786e465dc627d6efde6ed785f629b47b
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-21 22:45:43 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -57,7 +57,9 @@ export class App extends React.Component {
   render() {
     return (
       <div id="app">
-
+      <OlderCoaster />
+            <InFrontOfYou />
+            <ButcherShop />
       </div>
     )
   }
